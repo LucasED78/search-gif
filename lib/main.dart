5bf8017de0487b308c4c22eb30aef07e0e6bb69f
Lucas Eduardo <lucased78@hotmail.com>
2019-11-03 15:16:51 -0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:buscador_gifs/ui/homepage.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: 'Buscador de GIFS',
+    home: Home(),
+  ));
+}
---
+++ lib/ui/homepage.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'dart:async';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  
+  String searchTerm;
+  int offset;
+
+  Future<Map> _getGifs() async{
+    Response response;
+
+    if (searchTerm == null){
+      response = await Dio().get('https://api.giphy.com/v1/gifs/trending?api_key=QqlLOSG4m5xi5dBdfxNZqqp2Bav8J5DZ&limit=25&rating=G');
+    }
+    else response = await Dio().get('https://api.giphy.com/v1/gifs/search?api_key=QqlLOSG4m5xi5dBdfxNZqqp2Bav8J5DZ&q=$searchTerm&limit=25&offset=$offset&rating=G&lang=en');
+
+    return response.data;
+  }
+
+  _getItensCount(List data) => searchTerm == null ? data.length : data.length + 1;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _getGifs().then(print);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Image.network('https://developers.giphy.com/static/img/dev-logo-lg.7404c00322a8.gif'),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      body: Container(
+        padding: EdgeInsets.only(left: 10, right: 10, top: 10),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Pesquise Aqui",
+                labelStyle: TextStyle(color: Colors.white),
+                border: OutlineInputBorder()
+              ),
+              style: TextStyle(color: Colors.white, fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+            Expanded(
+              child: FutureBuilder(
+                future: _getGifs(),
+                builder: (context, snapshot){
+                  switch(snapshot.connectionState){
+                    case ConnectionState.none:
+                    case ConnectionState.waiting:
+                      return Container(
+                        alignment: Alignment.center,
+                        width: 200,
+                        height: 200,
+                        child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(Colors.white)),
+                      );
+                      break;
+                    default:
+                      return _buildGifsTable(context, snapshot);
+                  }
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildGifsTable(BuildContext context, AsyncSnapshot snapshot){
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10
+      ),
+      itemCount: _getItensCount(snapshot.data['data']),
+      itemBuilder: (context, index){
+        if (searchTerm == null || index < snapshot.data['data'].length){
+          return GestureDetector(
+            child: Image.network(snapshot.data['data'][index]['images']['fixed_height']['url']),
+          );
+        }
+        else {
+          return GestureDetector(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(Icons.add, color: Colors.white,),
+                Text('Carregar mais...', style: TextStyle(color: Colors.white, fontSize: 22), textAlign: TextAlign.center)
+              ],
+            ),
+            onTap: (){
+              setState(() => offset += 25);
+            },
+          );
+        }
+      },
+    );
+  }
+
+}
